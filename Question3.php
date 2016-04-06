1117a003a52b34c5a70d4173626a1753960ee191
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-04-06 06:28:56 -0400

    Pas encore fini

---
+++ Question3.php
@@ -0,0 +1,35 @@
+<html>
+
+<body>
+  <?php
+$Bookmarks = array(
+  array(
+    "name"=>"Moteur de recherche",
+    "links"=>array(
+        array("name"="Google", "url"=>"https://www.google.fr", "image"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Logo_Google_2013_Official.svg/64px-Logo_Google_2013_Official.svg.png"),
+        array("name"="Yahoo", "url"=>"https://fr.yahoo.com/", "image"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Yahoo%21_logo.svg/64px-Yahoo%21_logo.svg.png"),
+    ),
+   ),
+  array(
+    "name"=>"Journaux"
+    "links"=>array(
+      array("name"=>"Le Monde","url"=>"http://www.lemonde.fr/", "image"=>"http://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Le_monde_logo.svg/200px-Le_monde_logo.svg.png"),
+      array("name"=>"L'Equipe","url"=>"http://lequipe.fr/", "image"=>"http://upload.wikimedia.org/wikipedia/commons/3/32/L%27%C3%89quipe_wordmark.svg"),
+    )
+   
+ ),
+  array(
+    "name"=>"Sports"
+    "links"=>array(
+    array("name"=>"FFF", "url"=>"http://fff.fr/", "image"=>"http://upload.wikimedia.org/wikipedia/fr/6/67/Logo_F%C3%A9d%C3%A9ration_Fran%C3%A7aise_de_Football.svg"),
+    array("name"=>"FFDF", "url"=>"http://www.iut-fbleau.fr/sitebp/web/wim21/www.ffdf.fr", "image"=>"http://upload.wikimedia.org/wikipedia/fr/b/bf/FFDF.jpg"),
+  )
+  ),
+  );
+foreach($Bookmarks as $array){
+    
+  }
+?>
+</body>
+
+</html>
