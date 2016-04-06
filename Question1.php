9de08cfab8637c357b65f77bec6ea6f3fc437e1f
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-04-06 06:02:21 -0400

    second commit

---
+++ Question1.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+  <?php
+$clients = array (
+	"Luc", 
+	7 => "Paul", 
+	2 =>"Martin", 
+	"Arnaud"
+	);
+  echo "$clients[0]<br>"; /* 0 est l'entier servant de clé a Luc*/  
+  echo "$clients[8]"; /* 8 est l'entier servant de clé a Arnaud*/
+?>
+</body>
+
+</html>
---
+++ Question1suite.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+  <?php
+$produits = array (
+	20 => "Chemise",
+	3 => "Pantalon",
+	10 => "Jupe",
+	"Veste",
+	"Blouson"
+	);
+  echo "$produits[21]<br>"; /* 21 est l'entier servant de clé a Veste*/  
+  echo "$produits[22]"; /* 22 est l'entier servant de clé a Blouson*/
+?>
+</body>
+
+</html>
