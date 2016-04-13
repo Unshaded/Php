ec8b6fda27e0dd2208ab7d308d11640c5d0369a8
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-04-13 06:14:32 -0400

    Resultat dans l'operande 1

--- TD_2/TD_2_Q3.php
+++ TD_2/TD_2_Q3.php
@@ -1,3 +1,26 @@
+<?php
+  if(isset($_GET['Operande1'])){
+    $Operande1=$_GET['Operande1'];
+  }
+  if(isset($_GET['Operande2'])){
+    $Operande2=$_GET['Operande2'];
+  }
+
+  if(isset($_GET['Operation'])) {
+    $Operation=$_GET['Operation'];
+    if($Operation == 'Addition')
+      $res=$Operande1+$Operande2;
+    if($Operation == 'Soustraction')
+      $res=$Operande1-$Operande2;
+    if($Operation == 'Multiplication')
+      $res=$Operande1*$Operande2;
+    if($Operation == 'Division')
+      $res=$Operande1/$Operande2;
+  }
+
+  
+
+?>
 <html>
   <head>
     <meta charset='UTF-8'/>
@@ -9,41 +32,21 @@
         <legend>
           Calculatrice
         </legend>
-        <label>Operande 1</label>
-        <input type="text" name="Operande1">
+        <input type="text" name="Operande1" value="<?php if(isset($res)) echo $res;?>">
          <select name="Operation">
             <option value="Addition">+</option>
             <option value="Soustraction">-</option>
             <option value="Multiplication">*</option>
             <option value="Division">/</option>
         </select>
-        <label>Operande 2</label>
         <input type="text" name="Operande2">
+        <?php
+    if(isset($res))
+     echo " = ".$res;
+    ?>
       </fieldset>
       <input type="submit" name="Calculer">
     </form>
-    <?php
-    if(isset($_GET['Operande1'])){
-      $Operande1=$_GET['Operande1'];
-    }
-    if(isset($_GET['Operande2'])){
-      $Operande2=$_GET['Operande2'];
-    }
     
-    if(isset($_GET['Operation'])) {
-      $Operation=$_GET['Operation'];
-      if($Operation == 'Addition')
-        $res=$Operande1+$Operande2;
-      if($Operation == 'Soustraction')
-        $res=$Operande1-$Operande2;
-      if($Operation == 'Multiplication')
-        $res=$Operande1*$Operande2;
-      if($Operation == 'Division')
-        $res=$Operande1/$Operande2;
-    }
-
-    if(isset($res))
-       echo "Resultat=".$res;
-    ?>
   </body>
 </html>
