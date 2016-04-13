464e33f9da9d771f4b43b0dae5ed12cd10821d37
ozcan.ali94 <ozcan.ali94@gmail.com>
2016-04-13 05:57:20 -0400

    Question 3 premiere partie

---
+++ TD_2/TD_2_Q3.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <meta charset='UTF-8'/>
+    <title>Calculatrice</title>
+  </head>
+  <body>
+    <form action='TD_2_Q3.php' method='get'>
+      <fieldset>
+        <legend>
+          Calculatrice
+        </legend>
+        <label>Operande 1</label>
+        <input type="text" name="Operande1">
+         <select name="Operation">
+            <option value="Addition">+</option>
+            <option value="Soustraction">-</option>
+            <option value="Multiplication">*</option>
+            <option value="Division">/</option>
+        </select>
+        <label>Operande 2</label>
+        <input type="text" name="Operande2">
+      </fieldset>
+      <input type="submit" name="Calculer">
+    </form>
+    <?php
+    if(isset($_GET['Operande1'])){
+      $Operande1=$_GET['Operande1'];
+    }
+    if(isset($_GET['Operande2'])){
+      $Operande2=$_GET['Operande2'];
+    }
+    
+    if(isset($_GET['Operation'])) {
+      $Operation=$_GET['Operation'];
+      if($Operation == 'Addition')
+        $res=$Operande1+$Operande2;
+      if($Operation == 'Soustraction')
+        $res=$Operande1-$Operande2;
+      if($Operation == 'Multiplication')
+        $res=$Operande1*$Operande2;
+      if($Operation == 'Division')
+        $res=$Operande1/$Operande2;
+    }
+
+    if(isset($res))
+       echo "Resultat=".$res;
+    ?>
+  </body>
+</html>
